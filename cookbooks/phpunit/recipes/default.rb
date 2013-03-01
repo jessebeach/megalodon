1fb1019dd29bb838fb244a838fe08ba629146936
Mark Sonnabaum <mark@sonnabaum.com>
2013-03-01 14:40:22 -0600

    Added new symfony channel to phpunit recipe.

--- cookbooks/phpunit/recipes/default.rb
+++ cookbooks/phpunit/recipes/default.rb
@@ -16,7 +16,7 @@
 
 include_recipe "php"
 
-channels = %w{pear.phpunit.de components.ez.no pear.symfony-project.com}
+channels = %w{pear.phpunit.de components.ez.no pear.symfony.com pear.symfony-project.com}
 channels.each do |chan|
   php_pear_channel chan do
     action [:discover, :update]
